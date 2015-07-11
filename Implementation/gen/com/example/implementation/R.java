e2ba3a3c1085f28a8107046b8470933a07bb96f2
22avinash <arai08230@gmail.com>
2015-07-11 20:34:28 +0530

    test

--- Implementation/gen/com/example/implementation/R.java
+++ Implementation/gen/com/example/implementation/R.java
@@ -10,9 +10,6 @@ package com.example.implementation;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int colors=0x7f070000;
-    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -28,16 +25,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int btnSign=0x7f090004;
-        public static final int btnSignup=0x7f090003;
-        public static final int imageView1=0x7f090008;
-        public static final int lstSchools=0x7f090000;
-        public static final int txtBudget=0x7f090006;
-        public static final int txtPassword=0x7f090002;
-        public static final int txtSchoolName=0x7f090005;
-        public static final int txtSpent=0x7f090007;
-        public static final int txtUsername=0x7f090001;
+        public static final int action_settings=0x7f080009;
+        public static final int btnSign=0x7f080004;
+        public static final int btnSignup=0x7f080003;
+        public static final int imageView1=0x7f080008;
+        public static final int lstSchools=0x7f080000;
+        public static final int txtBudget=0x7f080006;
+        public static final int txtPassword=0x7f080002;
+        public static final int txtSchoolName=0x7f080005;
+        public static final int txtSpent=0x7f080007;
+        public static final int txtUsername=0x7f080001;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -45,7 +42,7 @@ public final class R {
         public static final int school_list_item=0x7f030002;
     }
     public static final class menu {
-        public static final int home=0x7f080000;
+        public static final int home=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050003;
