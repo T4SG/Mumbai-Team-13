7051fa8f6b1a6bb774fe09a194ab024567a6e803
22avinash <arai08230@gmail.com>
2015-07-11 16:41:30 +0530

    login

--- Implementation/src/com/example/implementation/MainActivity.java
+++ Implementation/src/com/example/implementation/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.implementation;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+    TextView txtUsername = (TextView)findViewById(R.id.txtUsername);
+    TextView txtPassword = (TextView)findViewById(R.id.txtPassword);
+    
+    Button btnSignIn = (Button)findViewById(R.id.btnSign);
+    
+    btnSignIn.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+        
     }
 }
